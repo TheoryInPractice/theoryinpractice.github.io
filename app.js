edb7611f5964d9d97341c94f4b3fabd5c7d53f1d
EricHorton <ewhorton@ncsu.edu>
2017-02-23 16:23:38 -0500

    Fixing templates for angular rendering.

--- app.js
+++ app.js
@@ -9,16 +9,60 @@
 angular
     .module('theory-in-practice', ['ui.router', 'ngRoute'])
     .config([
-        '$stateProvider', '$routeProvider',
-        function($stateProvider, $routeProvider) {
+        '$stateProvider', '$urlRouterProvider', '$uiViewScrollProvider',
+        function($stateProvider, $urlRouterProvider, $uiViewScrollProvider) {
 
             // Index Page
-            $stateProvider.state('index', {
+            $stateProvider.state('home', {
                 url: '/',
-                templateUrl: 'src/templates/index.html'
+                templateUrl: 'src/templates/home.html'
             });
 
-            // Default to index
-            $routeProvider.otherwise('/');
+            // About Page
+            $stateProvider.state('about', {
+                url: '/about',
+                templateUrl: 'src/templates/about.html'
+            });
 
+            // Events Page
+            $stateProvider.state('events-archive', {
+                url: '/events-archive',
+                templateUrl: 'src/templates/events-archive.html'
+            });
+
+            // News Page
+            $stateProvider.state('news', {
+                url: '/news',
+                templateUrl: 'src/templates/news.html'
+            });
+
+            // News Archive page
+            $stateProvider.state('news-archive', {
+                url: '/news-archive',
+                templateUrl: 'src/templates/news-archive.html'
+            });
+
+            // People Page
+            $stateProvider.state('people', {
+                url: '/people',
+                templateUrl: 'src/templates/people.html'
+            });
+
+            // Publications Page
+            $stateProvider.state('publications', {
+                url: '/publications',
+                templateUrl: 'src/templates/publications.html'
+            });
+
+            // Software Page
+            $stateProvider.state('software', {
+                url: '/software',
+                templateUrl: 'src/templates/software.html'
+            });
+
+            // Default to index if not found
+            $urlRouterProvider.otherwise('/');
+
+            // Configure anchor scroll to scroll to top on view change
+            $uiViewScrollProvider.useAnchorScroll()
     }]);
