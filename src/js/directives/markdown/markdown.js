fd170d1ea7f26660cffb0cd899516124c941b587
EricHorton <ewhorton@ncsu.edu>
2017-03-08 11:25:48 -0500

    Compiling rendered markdown contents using angular.

--- src/js/directives/markdown/markdown.js
+++ src/js/directives/markdown/markdown.js
@@ -5,12 +5,32 @@
  *
  * @module markdown.
  */
-angular.module('theory-in-practice').directive('markdown', [function() {
+angular.module('theory-in-practice').directive('markdown', ['$compile', function($compile) {
 
     return {
         restrict: 'E',
         template: '<div markdown-to-html="contents" class="markdown-body"></div>',
         scope: { contents: '=' },
+        link: function(scope, element) {
+
+            // Reference the markdown div that is has rendered html
+            // attached via ng-bind-html
+            var boundContents = angular
+                .element(element)
+                .children().first()
+                .children().first();
+
+            // Watch for markdown html to be changed and tell angular to compile it
+            scope.$watch(
+                function() {
+                    return boundContents.html();
+                },
+                function() {
+                    $compile(boundContents.contents())(scope);
+                }
+            );
+
+        }
     };
 
 }]);
