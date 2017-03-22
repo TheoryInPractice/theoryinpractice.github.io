bf39a73adb266c733e29374d66ee4be70430bb65
EricHorton <ewhorton@ncsu.edu>
2017-03-22 16:43:06 -0400

    Refactoring events directive to differentiate between upcoming and archived based on date.

--- src/js/directives/events/events.js
+++ src/js/directives/events/events.js
@@ -5,15 +5,22 @@
  */
 angular.module('theory-in-practice').directive('events', [function() {
 
-    const RECENT = 'recent';
-    const LIMIT  = 4;
+    const UPCOMING = 'upcoming';
 
     return {
         restrict: 'E',
         templateUrl: 'src/js/directives/events/events.html',
         scope: { contents: '=' },
         link: function(scope, element, attrs) {
-            scope.limit = (RECENT in attrs) ? LIMIT : undefined;
+
+            var now = new Date();
+
+            scope.upcoming = UPCOMING in attrs;
+            scope.items = scope.contents.filter(function(item) {
+                var start = new Date(item.start);
+                return scope.upcoming ? start > now : start < now;
+            });
+
         }
     };
 
