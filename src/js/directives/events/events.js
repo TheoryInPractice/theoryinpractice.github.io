ce8a8f20faeeb8bf07f32a578acc23252c12d2a0
Blair D. Sullivan <blair_sullivan@ncsu.edu>
2018-09-23 21:34:15 -0400

    Updating events directive to include things in the last 30 days as well as upcoming

--- src/js/directives/events/events.js
+++ src/js/directives/events/events.js
@@ -7,6 +7,7 @@ angular.module('theory-in-practice').directive('events', [function() {
 
     const UPCOMING = 'upcoming';
     const MONTHS = 3;
+    const PREV_MONTHS = 1;
 
     return {
         restrict: 'E',
@@ -14,18 +15,19 @@ angular.module('theory-in-practice').directive('events', [function() {
         scope: { contents: '=' },
         link: function(scope, element, attrs) {
 
-            var now = new Date();
+            var recent = new Date();
             var horizon = new Date();
             horizon.setMonth(now.getMonth() + MONTHS);
+            recent.setMonth(now.getMonth() - PREV_MONTHS);
 
             scope.upcoming = UPCOMING in attrs;
             scope.items = scope.contents.filter(function(item) {
                 var start = new Date(item.start);
                 if (scope.upcoming) {
-                    return start > now && start < horizon;
+                    return start > recent && start < horizon;
                 }
                 else {
-                    return start < now;
+                    return start < recent;
                 }
             });
 
