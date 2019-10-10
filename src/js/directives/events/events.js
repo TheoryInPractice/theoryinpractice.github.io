cd9d64368f5e880f944b89f11a6e138c1a95515d
Blair D. Sullivan <blair_sullivan@ncsu.edu>
2019-10-10 10:29:37 -0600

    changing date on dagstuhl to march and updating events feed to show prior 3 months

--- src/js/directives/events/events.js
+++ src/js/directives/events/events.js
@@ -7,7 +7,7 @@ angular.module('theory-in-practice').directive('events', [function() {
 
     const UPCOMING = 'upcoming';
     const MONTHS = 6;
-    const PREVMONTHS = 2;
+    const PREVMONTHS = 3;
 
     return {
         restrict: 'E',
